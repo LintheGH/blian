de7b26ce5674c1bc2f2782a35e41a20e2858cbce
linthegh <dzlgmyx@gmail.com>
2018-07-06 03:38:15 +0800

    modify by lin-h

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,18 @@ require.config({
     }
 })
 require(['jquery','generate','http'],function($,gen,http){
-    let list_details = $('.list-detail');
-    let lists = $('.all-list').find('li');
+    let my_ul = $('.adv-my-banner').find('ul')[0];
+    let gs_ul = $('.adv-gs-banner').find('ul')[0];
+    let my_lis = $('.adv-my-banner').find('li');
+    let gs_lis = $('.adv-gs-banner').find('li');
     let lis = $('.top-roll').find('li');
     let lics = $('.tr-idx').find('li');
     let timer;
+    let timer2;
+    let timer3;
     let idx = 1;
+    let idx2 = 1;
+    let idx3 = 1;
     let basemovel  = -160;
     let basemover = 160;
     let block = $('.b-lists')[0];
@@ -50,36 +56,63 @@ require(['jquery','generate','http'],function($,gen,http){
     //             list_details[i].style.display = 'none';
     //     }
     // });
-    
-    
-    function start(){
+    function troll(){
+        if(idx >= 7){
+            idx = 1;
+        }
+        for(let i=0;i<lis.length;i++){
+            if(lis[i].dataset.tridx == idx){
+                lis[i].style.opacity = 1;
+                lics[i].style.boxShadow = `0 0 0 4px #ccc`; 
+            }else{
+                lis[i].style.opacity = 0;
+                lics[i].style.boxShadow = `0 0 0 0`;
+            }
+        }
+        idx++;
+    }
+    function myroll(){
+        if(idx2 >= 4){
+            idx2 = 0;
+            my_ul.style.transition = 'all 0s';
+            my_ul.style.marginLeft = '0';
+        }
+        my_ul.style.marginLeft = `-${idx2*306}px`;
 
-        return setInterval(function(){
-            if(idx >= 7){
-                idx = 1;
-            }
-            for(let i=0;i<lis.length;i++){
-                if(lis[i].dataset.tridx == idx){
-                    lis[i].style.opacity = 1;
-                    lics[i].style.boxShadow = `0 0 0 4px #ccc`; 
-                }else{
-                    lis[i].style.opacity = 0;
-                    lics[i].style.boxShadow = `0 0 0 0`;
-                }
-            }
-            idx++;
-        },2000)
+        idx2++;
+    }
+    function gsroll(){
+        if(idx3 >= 4){
+            idx3 = 0;
+            gs_ul.style.transition = 'all 0s';
+            gs_ul.style.marginLeft = '0';
+        }else{
+            gs_ul.style.transition = 'all 1s' ;
+
+        }
+        gs_ul.style.marginLeft = `-${idx3*306}px`;
+        
+        idx3++;
     }
 
-    function stop(){
+
+
+    function start(fn){
+        return setInterval(function(){
+            fn();
+        },2000)
+    }
+    function stop(timer){
         clearInterval(timer);
     }
 
-    timer = start();
+    timer = start(troll);
+    timer2 = start(myroll);
+    timer3 = start(gsroll);
 
     for(let i=0;i<lics.length;i++){
         lics[i].onmouseover = function(){
-            stop();
+            stop(timer);
             for(let j=0;j<lics.length;j++){
                 if(this == lics[j]){
                      idx = lics[j].dataset.tridx;
@@ -92,9 +125,27 @@ require(['jquery','generate','http'],function($,gen,http){
             }
         }
         lics[i].onmouseout = function(){
-            timer = start();
+            timer = start(troll);
         }
     }
+    for(let i=0;i<gs_lis.length;i++){
+        gs_lis[i].onmouseover = function(){
+            stop(timer3);
+        }
+        gs_lis[i].onmouseout = function(){
+            timer3 = start(gsroll);
+        }
+    }
+    for(let i=0;i<my_lis.length;i++){
+        my_lis[i].onmouseover = function(){
+            stop(timer3);
+        }
+        my_lis[i].onmouseout = function(){
+            timer3 = start(myroll);
+        }
+    }
+
+
 
     $('.arrowl').on('click',function(){
         $('.arrowr')[0].style.display = 'block';
